970c80d923df744614f6942e23450017a636b223
Scott Weisman <sweisman18@gmail.com>
2012-12-09 05:07:38 -0600

    added scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 RailsVps::Application.routes.draw do
+  resources :tasks
+
+  root :to => 'tasks#index'
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
